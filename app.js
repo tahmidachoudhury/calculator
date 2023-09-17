0f634ba2983bbece056069b70714999ec23c768e
tahmidachoudhury <tahmidachoudhury@outlook.com>
2023-09-17 15:42:52 +0100

    changed the logic of calculator
    - now operates like iOS calculator
    - num1 and num2 are defined
    
    issues:
    - multiple strings can't be taken, "=" must be entered

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const digits = Array.from(document.querySelectorAll('.digit'));
 const displayScreen = document.querySelector('.display');
 
+//operate function-------------------------------------------------------------------
 add = function(a, b){
     return a + b;
 }
@@ -27,36 +28,61 @@ function operate(a, operator, b){
     if (operator in operations) return operations[operator](Number(a), Number(b));
     else return "invalid operator";
 }
+//---------------------------------------------------------------------------------------
 
+
+
+
+//calculator display --------------------------------------------------------------------
 let currentInput = "";
+let num1 = "";
 let operator = "";
+let num2 = "";
+
+function evalEquation(){
+    // let eqn = displayScreen.textContent.split(operator)
+    // currentInput = operate(eqn[0], operator, eqn[1]);
+    currentInput = operate(num1, operator, num2);
+    displayScreen.textContent = currentInput;
+}
+
+function clearScreen(){
+    currentInput = "";
+    displayScreen.textContent = "";
+}
 
 putNumbersOnScreen = function(e){
-    const buttonText = e.srcElement.textContent;
-    
-
+    const buttonText = e.srcElement.textContent;    
     if(buttonText === "CLR"){
-        currentInput = "";
-        displayScreen.textContent = "";
+        clearScreen();
 
     } else if(['+','-','÷','x'].includes(buttonText)){
+        if (num1 == ""){
+            num1 = displayScreen.textContent;
+            clearScreen();
+        } 
         operator = buttonText;
         currentInput += buttonText;
         displayScreen.textContent = currentInput;
+    
     } else if (buttonText === "="){
-        let eqn = displayScreen.textContent.split(operator)
-        currentInput = operate(eqn[0], operator, eqn[1]);
-        displayScreen.textContent = currentInput;
+        if(num2 == ""){
+            num2 = displayScreen.textContent;
+        }
+        evalEquation();
+        num1 = "";
+        num2 = "";
     } else {
+        if(['+','-','÷','x'].includes(displayScreen.textContent)){
+            clearScreen();
+        }
         currentInput += buttonText;
         displayScreen.textContent = currentInput;
-    }
-    
-    
+    }  
 }
 
 digit = document.querySelector('.buttons')
 digit.addEventListener('click', putNumbersOnScreen);
-
+// -----------------------------------------------------------------------------------------
 
 
